4ccc4e9316e64c202869b227a92d07d64ad1b4c6
anderson <and.dovale@gmail.com>
2025-04-15 15:54:30 -0300

    create Taksk 2

--- lib/components/Task.dart
+++ lib/components/Task.dart
@@ -5,21 +5,33 @@ class Task extends StatefulWidget {
   final String nome;
 
 
-  const Task(this.nome, {super.key});
+  const Task({required this.nome, Key? key}) : super(key: key);
 
   @override
   State<Task> createState() => _TaskState();
 }
 
 class _TaskState extends State<Task> {
+  bool _isDone = false; // Variável de estado para controlar se a tarefa está feita
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Row(
         children: [
-          const Icon(Icons.check_circle_outline),
+            IconButton(
+            icon: Icon(
+              _isDone ? Icons.check_box : Icons.check_box_outline_blank,
+            ),
+            onPressed: () {
+              setState(() {
+                _isDone = !_isDone; // Alterna o estado da tarefa
+              });
+            },
+          ),
           const SizedBox(width: 10),
-          Text(widget.nome),
+          Text(widget.nome, style: TextStyle( color: _isDone ? Colors.grey : Colors.black),),
 
         ],
       ),
